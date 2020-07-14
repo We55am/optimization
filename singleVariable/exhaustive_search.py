1853d864511b481b4b6a0ae69c8ada26e4c03c17
We55am <wessam.shoman@gmail.com>
2020-07-14 16:30:00 +0200

    first commit

---
+++ singleVariable/exhaustive_search.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import numpy as np
+import sympy as sym
+
+def main():
+    print "Single Variable -- Exhaustive Method"
+
+    ## Objective Function
+    print "Objective Function: "
+    x = sym.symbols('x')
+    F = (x**2) + (54/x)
+    print F
+    
+    ## Desgin Parameters
+    a = 0.5     # upper bound of the design variable
+    b = 5       # lower bound of the design variable
+    n = 100     # number of intermediate points
+
+    ## Exhaustive Search Method
+    delta_x = (b-a)/n
+    x1 = a
+    x2 = x1 + delta_x
+    x3 = x2 + delta_x
+
+    while (x3 < b):
+        f1 = F.subs(x, x1)
+        f2 = F.subs(x, x2)
+        f3 = F.subs(x, x3)
+        if (f2 <= f1 and f2 <= f3):
+            print "The minimum is: "
+            print x2
+            break
+        x1 = x2
+        x2 = x3
+        x3 = x3 + delta_x
+        
+
+    if (x3 == b):
+        print "couldn't find local minimum, consider changing the boundaires"
+
+        
+if __name__ == "__main__":
+    main()
